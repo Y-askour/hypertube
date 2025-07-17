1b385f5a8c142232730639e54faa406693e73970
Younes Askour <yaskouraskour@gmail.com>
2025-07-17 22:00:40 +0100

    switch to golang

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hypertube
+
+go 1.23.2
--- test/hypertube_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HypertubeTest do
-  use ExUnit.Case
-  doctest Hypertube
-
-  test "greets the world" do
-    assert Hypertube.hello() == :world
-  end
-end
